9c698faf6b4d53fd9f24dacb9141150b4c9e6cf5
doylecian <ciandoyle64@gmail.com>
2022-01-25 01:09:11 +0000

    Added URL routing to searchbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import Block from './Block.js';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar.js';
 import SearchBar from './SearchBar.js';
 import BlockPage from './BlockPage.js';
-
-const sampleData = [
-  { height: '0', 
-  blockhash: '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f', 
-  timestamp: '2022-01-22 18:41',
-  txcount: '1',
-  difficulty: '1.00',
-  version: '2022-01-22 18:41',
-  bits: '486,604,799',
-  size: '285' }
-];
 
 function App() {
   return (
@@ -22,7 +10,7 @@ function App() {
     <NavBar/>
     <SearchBar/>
       <Routes>
-        <Route path="/" element={<Block blockdata={sampleData}/>}/>
+        <Route path="/"/>
         <Route path="/tx/:tx" element={<BlockPage/>}/>
       </Routes>
     </div>
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,35 @@
 import './App.css';
+import React from 'react';
 
-function SearchBar() {
-  return (
-    <div className="SearchBarContainer">
-        <input type="text" className="SearchBar" placeholder="Search for a transaction, block or wallet"/>
-    </div>
-  );
+class SearchBar extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    
+    this.setState({value: event.target.value});  
+  }
+
+  handleSubmit(event) {
+    window.location = "http://localhost:3000/tx/" +  this.state.value;
+    event.preventDefault();
+  }
+  
+  render()
+  {
+    return (
+      <div className="SearchBarContainer">
+        <form action="" onSubmit={this.handleSubmit}>
+          <input type="text" className="SearchBar" value={this.state.value} onChange={this.handleChange} placeholder="Search for a transaction, block or wallet"/>
+        </form>
+      </div>
+    );
+  }
+  
 }
 
 export default SearchBar;
